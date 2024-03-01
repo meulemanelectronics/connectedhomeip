3942e7e10402726c3e69e0b6accbde645a4a6ad7
n-nievergeld <n.nievergeld@meuleman.io>
2024-03-01 13:54:01 +0100

    TSBM-23 Quickly push changes to connectedhomeip to get building

--- src/app/util/attribute-storage.cpp
+++ src/app/util/attribute-storage.cpp
@@ -14,7 +14,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
+#include <cstdint>
 #include <app/util/attribute-storage.h>
 
 #include <app/AttributeAccessInterfaceCache.h>
